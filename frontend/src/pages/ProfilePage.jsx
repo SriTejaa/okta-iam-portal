059142ca0d6c2188b8ad4395b28fc675cf649e2e
SaiKesa191 <kesa@agilitics.edu.sg>
2026-06-09 13:07:39 +0530

    feat(auth): add logout functionality

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 function ProfilePage() {
+    const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
   return (
     <div>
       <h1>Profile Page</h1>
       <p>Authenticated User Information</p>
+    <button onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 }
